c68a61a7c65584b809253605d5ac4b101d4a73fb
Gao quan <gaoquan@gloriousmed.com>
2020-03-25 13:37:03 +0800

    :art: 优化页面布局

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # o-ui
 学习造轮子 - vue组件库
+
+## yarn install
+## yarn serve
--- examples/App.vue
+++ examples/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div id="app">
-    <router-view />
+     <router-link to="/">splite-pane</router-link>
+     |
+     <router-link to="/menu-page">menu-page</router-link>
+     |
+     <router-link to="/message">message</router-link>
+     <div class="view">
+       <router-view/>
+     </div>
   </div>
 </template>
 <style lang="stylus">
-html, body, #app
-  margin 0
-  padding 0
+#app
   font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  .view
+    width 100%
+    min-height 80%
+    margin-top 20px
 </style>
